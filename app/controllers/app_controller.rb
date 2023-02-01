d16810e2f6c76337172d248ed795448935a15edc
Mohamed Atef <atefhamosa@gmail.com>
2023-02-01 04:13:08 -0800

    Add refresher

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,9 @@
 class AppController < ApplicationController
     def index
     end
+
+    def refresher
+        Net::HTTP.get(URI('https://appyrefresher.onrender.com/refresher'))
+        render json: 'I\'m good!'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
     end
   end
   get '*path', to: 'app#index'
+  get "/refresher", to: "app#refresher"
 end
